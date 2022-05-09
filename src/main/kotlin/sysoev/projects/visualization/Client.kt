2240b6633d873e6db6d54b87cbc330829a2097e5
Alexander.Sysoev <Alexander.Sysoev@jetbrains.com>
2022-05-10 00:55:58 +0200

    new api for root render

--- src/main/kotlin/sysoev/projects/visualization/Client.kt
+++ src/main/kotlin/sysoev/projects/visualization/Client.kt
@@ -2,13 +2,14 @@ package sysoev.projects.visualization
 
 import kotlinx.browser.document
 import react.create
-import react.dom.render
+import react.dom.client.createRoot
 
 fun main() {
     val container = document.getElementById("root") ?: error("Couldn't find root container!")
 
+    val root = createRoot(container)
     val welcome = Welcome.create {
         name = "Kotlin/JS"
     }
-    render(welcome, container)
+    root.render(welcome)
 }
